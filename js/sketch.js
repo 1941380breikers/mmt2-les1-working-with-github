a69b4fcd8e5bd3a78e71adce99357bdfb534ee28
1941380breikers <54942445+1941380breikers@users.noreply.github.com>
2020-01-15 12:08:47 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,36 @@
+function setup (){
+    createCanvas (500, 500);
+    background (240);
+}
+
+function draw (){
+    noStroke ();
+    fill (255, 0, 0);
+    rect (80, 150, 50, 50);
+
+    fill (255, 220, 30);
+    rect (150, 150, 50, 50);
+
+    fill (0, 250, 230);
+    rect (220, 150, 50, 50);
+
+    fill (0, 255, 30);
+    rect (290, 150, 50, 50);
+
+    fill (250);
+    stroke (0);
+    rect (80, 250, 50, 50);
+    
+    fill (180);
+    stroke (0);
+    rect (150, 250, 50, 50);
+    
+    fill (100);
+    stroke (0);
+    rect (220, 250, 50, 50);
+    
+    fill (50);
+    stroke (0);
+    rect (290, 250, 50, 50);
+    
+}
